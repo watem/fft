4d683583adf5bcf644d025015f623442fde94747
watem <watem.main@gmail.com>
2021-03-28 14:00:02 -0400

    setup and todos

---
+++ fft.py
@@ -0,0 +1,20 @@
+# TODO: input: -m Mode -i image
+
+
+
+# TODO: naive
+
+
+# TODO: fast
+
+
+# TODO: fft inverse
+
+# TODO: 2d-fft
+
+# TODO: 2d-fft inverse
+
+# TODO: 2d log scale plot
+
+
+# TODO: save dft to .txt or .csv
